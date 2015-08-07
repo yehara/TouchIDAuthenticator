878eb52b78d71bf87179f9369cc2f428cd487a26
yehara <yoshinori.ehara@gmail.com>
2015-08-07 18:43:13 +0900

    Initial Commit

---
+++ TouchIDApp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchIDApp2
+//
+//  Created by 江原 良典 on 2015/08/07.
+//  Copyright (c) 2015年 yehara.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
