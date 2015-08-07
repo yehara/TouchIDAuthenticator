ada42c1e357f15882aa608e13f883ac0b6042428
yehara <yoshinori.ehara@gmail.com>
2015-08-08 08:56:54 +0900

    add AppDelegate

--- TouchIDApp2/AppDelegate.swift
+++ TouchIDApp2/AppDelegate.swift
@@ -13,11 +13,20 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var iacDelegate: IACDelegate?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        iacDelegate = TouchIdDelegate()
+        IACManager.sharedManager().callbackURLScheme = "mijstouchid"
+        IACManager.sharedManager().delegate = iacDelegate
         return true
+    }
+
+    func application(application: UIApplication,
+        openURL url: NSURL,
+        sourceApplication: String?,
+        annotation: AnyObject?) -> Bool {
+            return IACManager.sharedManager().handleOpenURL(url)
     }
 
     func applicationWillResignActive(application: UIApplication) {
